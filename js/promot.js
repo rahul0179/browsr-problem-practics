16134f166b77fc23e10ce729d8940e25999a58e3
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-08-24 03:33:56 +0600

    problem-2 solve- prompt()

---
+++ js/promot.js
@@ -0,0 +1,10 @@
+const n1 = prompt("num1")
+const n2 = prompt("num2")
+
+let num1 = parseInt(n1);
+let num2 = parseInt(n2);
+console.log(num1)
+console.log(num2)
+const sum = num1 + num2 + 200;
+console.log(sum)
+alert(sum)
