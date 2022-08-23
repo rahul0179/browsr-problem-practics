eae1cbe2bafbb608a99823d22c554e33047a7b4b
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-08-24 03:10:43 +0600

    problem1-show output in 5s later

---
+++ js/timeOut.js
@@ -0,0 +1,17 @@
+let n1 = 100;
+let n2 = 200;
+let sum = n1 + n2
+
+console.log(n1);
+console.log(n2);
+setTimeout(() => {
+    console.log('Sum :', sum)
+}, 3500);
+
+const btn = () => {
+    setTimeout(() => {
+        alert('rone bone jole jungle ---jpy bab lok nath')
+    }, 5000);
+}
+
+
